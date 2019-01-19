3f264d453c6c6ebe6c6aa0eadcbe3adb2b004708
Marcin <marcingladkowski@gmail.com>
2019-01-19 11:07:17 +0100

    create data with interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface IProduct {
+  brand: string;
+  model: string;
+  promotion: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'devmeeting!';
+  public products: IProduct[] = [
+    {brand: 'Honda', model: 'Civic', promotion: false},
+    {brand: 'Porsche', model: '911', promotion: true},
+    {brand: 'Ferrari',  model: 'Diablo', promotion: false},
+  ];
+  public promotionProducts = this.products.filter((car) =>  car.promotion === true );
 }
