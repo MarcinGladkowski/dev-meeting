16bcde216263975c08e56f4b6bcd737c2fb0ccc8
Marcin <marcingladkowski@gmail.com>
2019-01-19 11:06:49 +0100

    initial new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppProductTitleComponent } from './app-product-title/app-product-title.component';
+import { AppProductListComponent } from './app-product-list/app-product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppProductTitleComponent,
+    AppProductListComponent,
   ],
   imports: [
     BrowserModule
